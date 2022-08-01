9e4797ee5c7349ec0d78bd0c6367ff028637dc50
yazentelci <110395677+yazentelci@users.noreply.github.com>
2022-08-01 15:22:04 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.canvas = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.canvas.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // canvas
+            // 
+            this.canvas.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.canvas.Controls.Add(this.button3);
+            this.canvas.Controls.Add(this.listBox1);
+            this.canvas.Controls.Add(this.button2);
+            this.canvas.Controls.Add(this.button1);
+            this.canvas.Location = new System.Drawing.Point(0, 0);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(800, 450);
+            this.canvas.TabIndex = 0;
+            this.canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseDown);
+            this.canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseMove);
+            this.canvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseUp);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(408, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(250, 95);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(474, 128);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "draw line from a file";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(332, 128);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "save to file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(654, 128);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.canvas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.canvas.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel canvas;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        Graphics graphics;
+        Boolean cursorMoving = false;
+        Pen cursorpen;
+        int cursorX = -1;
+        int cursorY = -1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            graphics =canvas.CreateGraphics();
+            cursorpen = new Pen(Color.Black, 7);
+            graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            cursorpen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+            cursorpen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            cursorMoving = true;
+            cursorX = e.X;
+            cursorY = e.Y;
+        }
+
+        private void canvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            cursorMoving = false;
+            cursorX = -1;
+            cursorY = -1;
+        }
+
+        private void canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(cursorX != -1 && cursorY != -1 && cursorMoving==true)
+            {
+                graphics.DrawLine(cursorpen, new Point(cursorX, cursorY), e.Location);
+                cursorX = e.X;
+                cursorY = e.Y;
+                listBox1.Items.Add( cursorX + ","+ cursorY);
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            if(dlg.ShowDialog()== DialogResult.OK)
+            {
+                StreamWriter writer = new StreamWriter(dlg.FileName);
+                for (int i = 0; i < listBox1.Items.Count;i++)
+                {
+                    writer.WriteLine((string)listBox1.Items[i]);
+                }
+                writer.Close();
+            }
+            dlg.Dispose();
+            MessageBox.Show("Programs saved!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string zee1, zee2, zee3, zee4;
+
+            double scordx, scordy, ecordx, ecordy;
+
+            int m1, n1, m2, n2;
+
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                using (StreamReader Reader = new StreamReader(openFileDialog1.FileName, true))
+                {
+
+                    foreach (string a in File.ReadAllLines(openFileDialog1.FileName))
+                    {
+                        zee1 = Reader.ReadLine();
+                        scordx = Convert.ToDouble(zee1);
+                        scordx = Math.Round(scordx);
+                        m1 = Convert.ToInt32(scordx);
+
+                        zee2 = Reader.ReadLine();
+                        scordy = Convert.ToDouble(zee2);
+                        scordy = Math.Round(scordy);
+                        n1 = Convert.ToInt32(scordy);
+
+                        zee3 = Reader.ReadLine();
+                        ecordx = Convert.ToDouble(zee3);
+                        ecordx = Math.Round(ecordx);
+                        m2 = Convert.ToInt32(ecordx);
+
+                        zee4 = Reader.ReadLine();
+                        ecordy = Convert.ToDouble(zee4);
+                        ecordy = Math.Round(ecordy);
+                        n2 = Convert.ToInt32(ecordy);
+                        Graphics formGraphics = this.CreateGraphics();
+                        graphics.DrawLine(cursorpen, m1, n1, m2, n2);
+                       
+                    }
+                    //cursorpen.Dispose();
+                    //graphics.Dispose();
+
+
+
+
+
+                }
+                   
+                }
+            }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            graphics.Clear(Color.White);
+        }
+    }
+    }
+
